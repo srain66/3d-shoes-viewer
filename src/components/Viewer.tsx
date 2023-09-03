81da5d224c6851f14ed3c3f2913191a96cc5f1dc
노이(현은비) <71617021+srain66@users.noreply.github.com>
2023-09-03 15:53:34 +0900

    feat: detect controls has moved

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -1,15 +1,18 @@
 import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
 
 export default function Viewer(): JSX.Element {
+  const [_hasMoved, setHasMoved] = useState(false);
+  const handleChange = () => setHasMoved(true);
+
   return (
-    <Canvas className="w-full h-full">
+    <>
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <pointLight />
       <mesh>
         <boxGeometry args={[1, 1, 1]} />
       </mesh>
-      <OrbitControls />
-    </Canvas>
+      <OrbitControls onChange={handleChange} />
+    </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Toolbar from "@/components/Toolbar";
 import Viewer from "@/components/Viewer";
-import { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useMemo, useState } from "react";
+import { Vector3 } from "three";
 
 export default function Home() {
+  const defaultPosition = useMemo(() => new Vector3(30, 40, 60), []);
+
   const [height, setHeight] = useState<number>();
 
   const handleHeightChange = (current: number) => setHeight(current);
@@ -10,7 +14,12 @@ export default function Home() {
   return (
     <div className="w-screen h-screen">
       <div className="W-full h-full" style={{ paddingBottom: height }}>
-        <Viewer />
+        <Canvas
+          className="w-full h-full"
+          camera={{ position: defaultPosition }}
+        >
+          <Viewer />
+        </Canvas>
       </div>
       <Toolbar onHeightChange={handleHeightChange} />
     </div>
