b6e0fe7c85e784040c3d3f4da955024063661ea3
노이(현은비) <71617021+srain66@users.noreply.github.com>
2023-09-05 15:43:36 +0900

    style: add blank line between

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,6 +4,7 @@ import { HTMLAttributes, ReactNode } from "react";
 interface IProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
 }
+
 export default function Layout({ children, ...props }: IProps): JSX.Element {
   return (
     <div {...props}>
